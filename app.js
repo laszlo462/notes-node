f668bac896b21bdd870969e9eb36069f8955c1fa
Steve Szabo <laszlo.steve@gmail.com>
2018-09-19 20:17:47 -0500

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('Starting app.js')
+
+const fs = require('fs')
+const os = require('os')
+const notes = require('./notes')
+
+var res = notes.addNote()
+console.log(res)
+
+// var user = os.userInfo()
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`)
