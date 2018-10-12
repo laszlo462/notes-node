1eab9211700fa04177f59e0b4da9a7101a3f937c
Steve Szabo <laszlo.steve@gmail.com>
2018-10-11 19:31:08 -0500

    fixed it

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('Starting app.js')
 
 const fs = require('fs')
 const os = require('os')
-const notes = require('./notes')
+const notes = require('./notes.js')
 
 var res = notes.addNote()
 console.log(res)
