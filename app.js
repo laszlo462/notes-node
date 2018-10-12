ae9d57aa4934c990df4051aaa4d1ad4c79f75b88
Steve Szabo <laszlo.steve@gmail.com>
2018-10-11 20:23:49 -0500

    Lecture complete

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ console.log('Starting app.js')
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes.js')
 
+// console.log(_.isString(true))
+// console.log(_.isString('Steve'))
+var filteredArray = _.uniq(['Steve', 1, 'Steve', 1, 2, 3, 4])
+console.log(filteredArray)
+
 // var sum = notes.add(27, 10)
-console.log('Result:', notes.add(12, 8))
+// console.log('Result:', notes.add(12, 8))
 
 // var res = notes.addNote()
 // console.log(res)
