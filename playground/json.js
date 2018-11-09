24f9c05366ad17eeba2fae4d1065f265f23359cc
Steve <laszlo.steve@gmail.com>
2018-11-08 21:48:00 -0600

    halfway through video

---
+++ playground/json.js
@@ -0,0 +1,11 @@
+//var obj = {
+//    name: "Steve"
+//}
+//var stringObj = JSON.stringify(obj)
+//console.log(typeof stringObj)
+//console.log(stringObj)
+
+var personString = '{"name": "Steve", "age": 33}'
+var person = JSON.parse(personString)
+console.log(typeof person)
+console.log(person)
